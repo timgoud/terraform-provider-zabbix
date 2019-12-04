ba376707fe558e4420031f130fa4eafae2473f81
gaetan <gaetan.depin@fr.clara.net>
2019-12-04 11:19:39 +0100

    add lld_rule_link exemple

--- examples/linux_general/lld_rule.tf
+++ examples/linux_general/lld_rule.tf
@@ -30,3 +30,14 @@ resource "zabbix_trigger_prototype" "test_trigger_prototype" {
   expression = "{${zabbix_template.base_linux_general.host}:${zabbix_item_prototype.test_item_prototype.key}.last()}=0"
   priority = 5
 }
+
+
+resource "zabbix_lld_rule_link" "test_link" {
+  lld_rule_id = zabbix_lld_rule.test_lld_rule.id
+  item_prototype {
+    item_id = zabbix_item_prototype.test_item_prototype.id
+  }
+  trigger_prototype {
+    trigger_id = zabbix_trigger_prototype.test_trigger_prototype.id
+  }
+}
